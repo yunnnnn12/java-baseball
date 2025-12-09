42e8925782dedef275e3a402f560ee0206930869
chaeyun_ <sonchaeyun@sonchaeyun-ui-MacBookAir.local>
2025-12-09 16:04:53 +0900

    feat: add RandNumTest

--- src/test/java/baseball/ServiceTest/RandNumTest.java
+++ src/test/java/baseball/ServiceTest/RandNumTest.java
@@ -1,2 +1,21 @@
-package baseball.ServiceTest;public class RandNumTest {
+package baseball.ServiceTest;
+
+import baseball.service.BaseballService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class RandNumTest {
+    @DisplayName("랜덤 넘버 조건 테스트")
+    @Test
+    void checkRandomNumberSize(){
+        BaseballService baseballService = new BaseballService();
+        List<Integer> randNum = baseballService.makeRandNum();
+
+        assertThat(randNum).hasSize(3);
+    }
 }
