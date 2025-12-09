1bbbbea193acb726405a5b3ce602bf6b97a9a092
chaeyun_ <sonchaeyun@sonchaeyun-ui-MacBookAir.local>
2025-12-09 16:03:14 +0900

    feat: add validator

--- src/main/java/baseball/validate/InputNumValidator.java
+++ src/main/java/baseball/validate/InputNumValidator.java
@@ -1,25 +1,25 @@
 package baseball.validate;
 
-public class InputValidator {
+public class InputNumValidator {
     public static void validate(String nums){
         validateLength(nums);
-        IfDuplite(nums);
-        ValidateNumericAndRange(nums);
+        ifDuplite(nums);
+        validateNumericAndRange(nums);
     }
 
-    private static void validateLength(String nums){
+    public static void validateLength(String nums){
         if(nums.length() != 3){
             throw new IllegalArgumentException("3자리 숫자여야 합니다.");
         }
     }
 
-    private static void IfDuplite(String nums){
+    public static void ifDuplite(String nums){
         if(nums.length() != nums.chars().distinct().count()){
             throw new IllegalArgumentException("값들은 중복되지 않아야 합니다.");
         }
     }
 
-    private static void ValidateNumericAndRange(String nums){
+    public static void validateNumericAndRange(String nums){
         if(!nums.matches("[1-9]+")){
             throw new IllegalArgumentException("1~9범위의 숫자여야 합니다.");
         }
